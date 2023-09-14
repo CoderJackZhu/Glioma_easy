6a1a69fd6e927f90614395b26cf00d8dc4e828ac
spgou <zhuyijie151@sina.com>
2023-09-14 13:02:47 +0800

    add some results

--- utils/opts.py
+++ utils/opts.py
@@ -16,7 +16,7 @@ parser.add_argument('--val_list', default='/media/spgou/DATA/ZYJ/Glioma_easy/dat
 parser.add_argument('--model_name', type=str, default="resnet50")
 parser.add_argument('--model_depth', type=int, default=34, help='the depth of the model')
 parser.add_argument('--is_pretrained',default=False, action="store_true", help='using imagenet pretrained model')
-parser.add_argument('--tune_from', type=str, default='/media/spgou/DATA/ZYJ/Glioma_easy/outputs/precision_0.6264_num_95/epoch_95.pth', help='fine-tune from checkpoint')
+parser.add_argument('--tune_from', type=str, default='/media/spgou/DATA/ZYJ/Glioma_easy/all_roi_outputs/precision_0.6552_num_30/epoch_30.pth', help='fine-tune from checkpoint')
 parser.add_argument('--focal-loss', default=False, action="store_true")
 parser.add_argument('--label-smooth', default=False, action="store_true")
 parser.add_argument('--resume', type=str, default='', help='resume from checkpoint')
